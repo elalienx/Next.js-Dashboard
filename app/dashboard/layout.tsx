7e666eb390fbe57d228a3b9c83174ab912d7ef21
Eduardo Alvarez <el_alienx@hotmail.com>
2024-08-31 16:49:53 +0200

    added Partial Page Rendering (PPR)

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,9 @@
 // Project files
 import SideNav from "@/app/ui/dashboard/sidenav";
 
+// Enable Partial Page Rendenring (PPR)
+export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
