aebed42f7c2c202534dc4b9cf1676f228715be95
Eduardo Alvarez <el_alienx@hotmail.com>
2024-08-31 11:12:29 +0200

    added notes about layout in the dashboard layout.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 
+/**
+ * One benefit of using layouts in Next.js is that on navigation,
+ * only the page components update while the layout won't re-render.
+ * This is called partial rendering. See diagram:
+ * https://nextjs.org/_next/image?url=%2Flearn%2Flight%2Fpartial-rendering-dashboard.png&w=3840&q=75
+ */
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
