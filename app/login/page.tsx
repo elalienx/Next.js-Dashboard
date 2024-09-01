120ee896855144ac7100beebdb27ac3b35db1e14
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-01 18:46:15 +0200

    added env

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
 
 export const metadata: Metadata = {
-  title: "Login Auth with Env vars | Acme Dashboard",
+  title: "Login Auth v2 | Acme Dashboard",
 };
 
 export default function LoginPage() {
