e251b851d7e245c8e9d0359df7370f69182f4cfc
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-01 18:42:50 +0200

    added title to force new deployment and check the env variables in production

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
 
 export const metadata: Metadata = {
-  title: "Login | Acme Dashboard",
+  title: "Login Auth | Acme Dashboard",
 };
 
 export default function LoginPage() {
